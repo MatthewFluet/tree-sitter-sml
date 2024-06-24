99de5f0f3ec7d9829e177504c15da41f000b98dd
Matthew Fluet <matthew.fluet@gmail.com>
2024-06-24 17:11:58 -0400

    Formatting

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -54,10 +54,8 @@
 ;; *******************************************************************
 
 ;; Assume value identifiers starting with capital letter are constructors
-((vid) @constructor
- (#match? @constructor "^[A-Z].*"))
-(longvid ((vid) @vid
-          (#match? @vid "^[A-Z].*"))) @constructor
+((vid) @vid (#match? @vid "^[A-Z].*")) @constructor
+(longvid ((vid) @vid (#match? @vid "^[A-Z].*"))) @constructor
 
 ;; "true", "false", "nil", "::", and "ref" are built-in constructors
 ((vid) @vid (#any-of? @vid "true" "false" "nil" "::" "ref")) @constant.builtin
